f0d6a3a6be470907408d3bbf03f7e849e88e0dbd
Fanayzam <fanayzam@MacBook-Air-Fanayzam.local>
2023-11-24 03:12:41 +0300

    step 5 end

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 [![Actions Status](https://github.com/Fanayzam/python-project-49/workflows/hexlet-check/badge.svg)](https://github.com/Fanayzam/python-project-49/actions)
 
 <a href="https://codeclimate.com/github/Fanayzam/python-project-49/maintainability"><img src="https://api.codeclimate.com/v1/badges/493dbf6eddb66ebca098/maintainability" /></a>
+
+https://asciinema.org/a/wAZVHayiiC8MIH0hC1NSSYTAu
--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -3,24 +3,24 @@
 from random import randint
 import prompt
 
-
-ran = 0
-count = 0
-print('Welcome to the Brain Games!')
-name = prompt.string('May I have your name? ')
-print("Hello, " + str(name) + "!")
-print('Answer "yes" if the number is even, otherwise answer "no".')
-while count < 3:
-  ran = randint(1, 100)
-  answer = prompt.string('Question: '+ str(ran) + '\nYour answer: ')
-  if (answer == 'yes' and int(ran) % 2 == 0) or (answer == 'no' and int(ran) % 2 != 0):
-      print('Correct!')
-      count += 1
-  else:
-      print("'yes' is wrong answer ;(. Correct answer was 'no'.'\nLet's try again, " + str(name) + "!")
-      break
-if count == 3:
-  print("Congratulations, " + str(name) + "!")
+def main():
+    ran = 0
+    count = 0
+    print('Welcome to the Brain Games!')
+    name = prompt.string('May I have your name? ')
+    print("Hello, " + str(name) + "!")
+    print('Answer "yes" if the number is even, otherwise answer "no".')
+    while count < 3:
+      ran = randint(1, 100)
+      answer = prompt.string('Question: '+ str(ran) + '\nYour answer: ')
+      if (answer == 'yes' and int(ran) % 2 == 0) or (answer == 'no' and int(ran) % 2 != 0):
+          print('Correct!')
+          count += 1
+      else:
+          print("'yes' is wrong answer ;(. Correct answer was 'no'.'\nLet's try again, " + str(name) + "!")
+          break
+    if count == 3:
+      print("Congratulations, " + str(name) + "!")
 
 if __name__ == '__main__':
     main()
